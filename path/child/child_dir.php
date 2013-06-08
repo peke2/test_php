2bba7291188fa67740ec619c5bc7e69072999c78
U-92a-abe\92 <92@92a-abe.(none)>
2013-06-08 16:57:30 +0900

    実行時のカレントディレクトリの扱いなどについてまとめを追加。 カレントディレクトリによらず読み込みができることを確認できるコードを追加。

--- path/child/child_dir.php
+++ path/child/child_dir.php
@@ -4,6 +4,7 @@
 echo	"dirname = ".dirname(__FILE__)."\n";
 echo	"getcwd  = ".getcwd()."\n";
 
-require("../inc.php");
+//require("../inc.php");	//	相対パスだと実行時のカレントディレクトリに依存するので失敗することもある
+require( dirname(__FILE__)."/../inc.php" );	//	ファイルの絶対パスを基準に読み込めば、カレントディレクトリに依存しない読み込みが可能
 
 ?>
